2bf839802781c3dc9f24e58c5ef586dfb598738c
Nicholas Chai <nicholas.chai@outlook.com>
2023-09-12 19:44:28 +0800

    decoupled rendering the dice result to the page from the roll dice function to allow more flexibility with the target element created a roll dice function test that executes when the script first executes

--- Labs/Lab_1/js/dice.js
+++ Labs/Lab_1/js/dice.js
@@ -1,10 +1,38 @@
-function RollDice(faces){
+function RollDice(max){
     let min = 1;
     let randomNumber = Math.random();
-    let result = Math.floor((randomNumber * faces) + min)
+    let result = Math.floor((randomNumber * max) + min);
 
-    let outputMessage = "D" + faces + ": " + result;
-    document.getElementById("resultOutput").innerHTML = outputMessage;
-    
     return result;
 }
+
+function RenderDiceResult(faces, targetElementID){
+    let result = RollDice(faces);
+    let outputMessage = "D" + faces + ": " + result;
+    document.getElementById(targetElementID).innerHTML = outputMessage;
+}
+
+function DoesRollDiceWork(){
+    let diceValue;
+    let d6works;
+    let d10works;
+
+    diceValue = RollDice(6);
+    if (diceValue <= 6 && diceValue >= 1){
+        d6works = true;
+    }
+
+    diceValue = RollDice(10);
+    if (diceValue <= 10 && diceValue >= 1){
+        d10works = true;
+    }
+
+    if (d6works && d10works){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+let testResult = DoesRollDiceWork();
+console.log("roll dice test result: " + testResult);
