79bcd38139d3dc3ae7b0f2fbe1a8393586d419f4
Nicholas Chai <nicholas.chai@outlook.com>
2023-10-06 12:41:06 +0000

    question 2 advanced lab completed

--- Labs/Module_3/Lab_1/Javascript_Advanced.js
+++ Labs/Module_3/Lab_1/Javascript_Advanced.js
@@ -16,4 +16,19 @@ counter1(); // 2
 let counter2 = makeCounter(3, 2)
 counter2()
 
-// Question 2. 
+// Question 2. 2. The following delayMsg function is intended to be used to delay printing a message until some time has passed.
+//a) What order will the four tests below print in? Why?
+// answer: 4,3,2,1 cos of the settimeout delay
+//b) Rewrite delayMsg as an arrow function
+//c) Add a fifth test which uses a large delay time (greater than 10 seconds)
+//d) Use clearTimeout to prevent the fifth test from printing at all.
+
+delayMsg = (msg) => {
+    console.log(`This message will be printed after a delay: ${msg}`)
+}
+setTimeout(delayMsg, 100, '#1: Delayed by 100ms');
+setTimeout(delayMsg, 20, '#2: Delayed by 20ms');
+setTimeout(delayMsg, 0, '#3: Delayed by 0ms');
+delayMsg('#4: Not delayed at all')
+let id = setTimeout(delayMsg, 10000, '#3: Delayed by 0ms');
+clearTimeout(id);
