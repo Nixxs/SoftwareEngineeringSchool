f613526206be6d3f06d417602e83dd090b9da558
Nicholas Chai <nicholas.chai@outlook.com>
2023-10-31 11:30:07 +0800

    actually now it's done, I didn't fulfill the requirement for making it run every 5 seconds before, this has now been implemented.

--- Labs/Module_4/Lab_5/js/script.js
+++ Labs/Module_4/Lab_5/js/script.js
@@ -1,13 +1,46 @@
-let stories = [
-  { id: 1, title: "Election Results", content: "Newly elected minister..." },
-  { id: 2, title: "Sporting Success", content: "World Cup winners..." },
-  { id: 3, title: "Tornado Warning", content: "Residents should prepare..." },
-];
+class News {
+    constructor() {
+        this.stories = [
+            {
+                id: 1,
+                title: "Election Results",
+                content: "Newly elected minister...",
+            },
+            {
+                id: 2,
+                title: "Sporting Success",
+                content: "World Cup winners...",
+            },
+            {
+                id: 3,
+                title: "Tornado Warning",
+                content: "Residents should prepare...",
+            },
+        ];
+    }
 
-function updateNews(){
-    for (let story in stories){
-        let storyTitle = stories[story].title;
-        let storyContent = stories[story].title;
+    addStory(title, content) {
+        let allIDs = this.stories.map(function (story) {
+            return story.id;
+        });
+        let maxID = Math.max(allIDs);
+
+        let newStory = {
+            id: (maxID += 1),
+            title: title,
+            content: content,
+        };
+        this.stories.push(newStory);
+    }
+}
+let news = new News();
+
+function updateNews() {
+
+    document.querySelector("#news-list").innerHTML = "";
+    for (let story in news.stories) {
+        let storyTitle = news.stories[story].title;
+        let storyContent = news.stories[story].title;
 
         const template = document
             .getElementById("news-template")
@@ -19,3 +52,4 @@ function updateNews(){
     }
 }
 updateNews();
+setInterval(updateNews, 5000);
